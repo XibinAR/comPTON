e6b36c9d93869cd66d50eebccbcdcf28f960830d
XibinAR <95364909+XibinAR@users.noreply.github.com>
2023-04-10 19:34:45 +0000

    Développeur Java / Python (H/F)

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index'
-i
+import Technical from '../views/Technical'
+import NotFound from '../views/NotFound'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component:Index
+  },
+  {
+    path:'/technical',
+    name:'technical',
+    component:Technical
+  },
+  {
+    path:'*',
+    name:'notfound',
+    component:NotFound
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
