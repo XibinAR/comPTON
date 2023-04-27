f5efcefaedd4f15ff76fbdd85136430d3ba17c13
XibinAR <95364909+XibinAR@users.noreply.github.com>
2023-04-27 11:27:53 +0000

    missing change

---
+++ src/views/Technical.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+        <v-alert type="error" outlined v-if='this.$store.state.error' dismissible>
+          API Error
+    </v-alert>
+    <v-alert type="warning" outlined v-if='!this.$store.state.loaded' dismissble>
+        API Timeout
+    </v-alert>
+    <v-row>
+        <v-col>
+            <v-card outlined class="blue-grey darken-1" dark>
+                 <v-card-title><span class="overline">{{this.$store.state.symbol}}</span><v-spacer/><span class="overline">Volume</span></v-card-title>
+                <v-card-text class="headline">
+                    {{this.$store.state.vol
