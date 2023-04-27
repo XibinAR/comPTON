385320dc85c191b8f0d0a6a188cd2f2e7ef38f09
XibinAR <95364909+XibinAR@users.noreply.github.com>
2023-04-27 17:24:58 +0000

    finishing up

--- src/views/Technical.vue
+++ src/views/Technical.vue
@@ -11,4 +11,18 @@
             <v-card outlined class="blue-grey darken-1" dark>
                  <v-card-title><span class="overline">{{this.$store.state.symbol}}</span><v-spacer/><span class="overline">Volume</span></v-card-title>
                 <v-card-text class="headline">
-                    {{this.$store.state.vol
+                    {{this.$store.state.volume.status}}
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col>
+            <v-card outlined class="blue-grey darken-1" dark> 
+                <v-card-title><span class="overline">{{this.$store.state.symbol}}</span><v-spacer/><span class="overline">Percent</span></v-card-title>
+                <v-card-text class="headline">
+                    {{this.$store.state.percent}}
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col>
+            <v-card outlined class="blue-grey darken-1" dark>
+                    <v-card-tit
