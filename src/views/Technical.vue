811b298240c0cfce4515d7ab5b438ea3f733dab5
XibinAR <95364909+XibinAR@users.noreply.github.com>
2023-05-11 20:50:52 +0000

    v3.3.0

--- src/views/Technical.vue
+++ src/views/Technical.vue
@@ -50,4 +50,14 @@
                 data: this.$store.state.ema.data
                     }]" v-if="this.$store.state.loaded" ref='average'/>
             </v-col>
-            <v-
+            <v-col lg=4 md=4>
+                 <v-card outlined class="blue-grey darken-1 mb-5" dark>
+                        <v-card-title><span class="overline">{{this.$store.state.symbol}}</span><v-spacer/><span class="overline">Percent Change</span></v-card-title>
+                    <v-card-text class="headline">
+                        {{this.$store.state.percentchange}}
+                    </v-card-text>
+                </v-card>
+              <v-data-table
+                :headers="headers"
+                :items="this.$store.getters.volumeTable"
+                :items-per-page="1
